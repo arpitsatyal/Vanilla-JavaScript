17f4a624b3c9012262648eec88e5adb64eac1035
Arpit Satyal <arpited7@gmail.com>
2020-03-05 18:41:20 +0545

    call and apply

--- functions and objects/apply.js
+++ functions and objects/apply.js
@@ -1,4 +1,4 @@
-function calcMax(arr,arr2, arr3) {
+function getArr(arr,arr2, arr3) {
 	return [arr,arr2,arr3].length
 }
 
@@ -6,9 +6,14 @@ function calcMax(arr,arr2, arr3) {
 // 	return arr // output = 1
 // }
 
-
-
-console.log(calcMax.apply(null, [1,2,3,4,5]))
+console.log(getArr.apply(null, [1,2,3,4,5]))
 
 // array's each element is treated as individual argument to the function. so if I pass only one argument to the function,
 // I will get the output "1"
+
+
+let numbers = [1,2,3,4,5]
+
+let max = Math.max.apply(null, numbers)
+
+console.log(max)
