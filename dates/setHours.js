7d8cd9a421a578de7be4ee67745b0453d48575dd
Arpit Satyal <arpited7@gmail.com>
2020-03-10 22:08:46 +0545

    set hours

---
+++ dates/setHours.js
@@ -0,0 +1,11 @@
+let now = new Date('March 10 2020')
+now.setHours(10,5,43)
+console.log(now)
+
+// OUTPUT = Tue Mar 10 2020 10:05:43 GMT+0545 (Nepal Time)
+
+// sets hours/min/sec to the date object
+// 1st argmument = hour
+// 2nd = min, 3rd = sec
+
+// by default the hr:min:sec in the date object is 00:00:00
