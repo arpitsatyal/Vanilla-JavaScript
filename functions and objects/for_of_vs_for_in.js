2fbfb06c0f566ddf1911590248ec7f98cabf8298
Arpit Satyal <arpited7@gmail.com>
2021-07-14 15:19:59 +0545

    for OF vs for IN

---
+++ functions and objects/for_of_vs_for_in.js
@@ -0,0 +1,31 @@
+var obj = {
+    name: 'ram',
+    address: 'tinkune',
+    phone: 43444,
+    email: 'q@c',
+    ram:'hi'
+};
+
+let arr = [1,2,3,4,5]
+
+// for in loop
+// loops over all the keys in the object
+for (let key in obj) {
+	console.log(key)
+	console.log(obj[key])
+} 
+
+for (key in arr) {
+	console.log(key) //prints all the indexes of the array
+}
+
+for (let x of arr) {
+	console.log(x) //prints all the values of the array
+}
+
+// for of loop
+for (let x of obj) {
+	console.log(x) 
+}
+// says obj is not iterable
+
